5cf618f995745b175509883fcfbbac8bacd782c2
Agus Pujianto <53058544+masagus31@users.noreply.github.com>
2023-05-11 21:27:29 +0700

    Update sansekai.js

--- sansekai.js
+++ sansekai.js
@@ -20,43 +20,61 @@ const BOT_NAME = process.env.BOT_NAME ?? "Lily Shania";
 
 module.exports = sansekai = async (client, m, chatUpdate, store) => {
     try {
-        var body = (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : (m.mtype === 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) : ''
-        var budy = (typeof m.text == 'string' ? m.text : '')
-        var prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : "/"
-        const isCmd2 = body.startsWith(prefix)
-        const command = body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase()
-        const args = body.trim().split(/ +/).slice(1)
-        const pushname = m.pushName || "No Name"
-        const botNumber = await client.decodeJid(client.user.id)
-        const itsMe = m.sender == botNumber ? true : false
-        let text = q = args.join(" ")
-        const arg = budy.trim().substring(budy.indexOf(' ') + 1)
-        const arg1 = arg.trim().substring(arg.indexOf(' ') + 1)
-
-        console.log(m);
+        var body = (m.mtype === 'conversation') 
+        ? m.message.conversation 
+        : (m.mtype == 'imageMessage') 
+        ? m.message.imageMessage.caption 
+        : (m.mtype == 'videoMessage') 
+        ? m.message.videoMessage.caption 
+        : (m.mtype == 'extendedTextMessage') 
+        ? m.message.extendedTextMessage.text 
+        : (m.mtype == 'buttonsResponseMessage') 
+        ? m.message.buttonsResponseMessage.selectedButtonId 
+        : (m.mtype == 'listResponseMessage') 
+        ? m.message.listResponseMessage.singleSelectReply.selectedRowId 
+        : (m.mtype == 'templateButtonReplyMessage') 
+        ? m.message.templateButtonReplyMessage.selectedId 
+        : (m.mtype === 'messageContextInfo') 
+        ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) 
+        : ''
+        
+        var budy = typeof m.text == "string" ? m.text : "";
+        var prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : "/";
+        const isCmd2 = body.startsWith(prefix);
+        const command = body.replace(prefix, "").trim().split(/ +/).shift().toLowerCase();
+        const args = body.trim().split(/ +/).slice(1);
+        const pushname = m.pushName || "No Name";
+        const botNumber = await client.decodeJid(client.user.id);
+        const itsMe = m.sender == botNumber ? true : false;
+        let text = (q = args.join(" "));
+        const arg = budy.trim().substring(budy.indexOf(" ") + 1);
+        const arg1 = arg.trim().substring(arg.indexOf(" ") + 1);
+        // console.log(m);
     
-        const from = m.chat
-        const reply = m.reply
-        const sender = m.sender
-        const mek = chatUpdate.messages[0]
+        const from = m.chat;
+        const reply = m.reply;
+        const sender = m.sender;
+        const mek = chatUpdate.messages[0];
 
         const color = (text, color) => {
-            return !color ? chalk.green(text) : chalk.keyword(color)(text)
-        }
+            return !color ? chalk.green(text) : chalk.keyword(color)(text);
+        };
 
         // Group
         const groupMetadata = m.isGroup ? await client.groupMetadata(m.chat).catch(e => { }) : ''
         const groupName = m.isGroup ? groupMetadata.subject : ''
 
         // Push Message To Console
-        let argsLog = (budy.length > 30) ? `${q.substring(0, 30)}...` : budy
+        // let argsLog = (budy.length > 30) ? `${q.substring(0, 30)}...` : budy
+        // Push Message To Console
+        let argsLog = budy.length > 30 ? `${q.substring(0, 30)}...` : budy;
 
         if (setting.autoAI) {
             // Push Message To Console && Auto Read
-            if (argsLog && !m.isGroup) {
+            if (isCmd2 && !m.isGroup) {
                 // client.sendReadReceipt(m.chat, m.sender, [m.key.id])
                 console.log(chalk.black(chalk.bgWhite('[ LOGS ]')), color(argsLog, 'turquoise'), chalk.magenta('From'), chalk.green(pushname), chalk.yellow(`[ ${m.sender.replace('@s.whatsapp.net', '')} ]`))
-            } else if (argsLog && m.isGroup) {
+            } else if (isCmd2 && m.isGroup) {
                 // client.sendReadReceipt(m.chat, m.sender, [m.key.id])
                 console.log(chalk.black(chalk.bgWhite('[ LOGS ]')), color(argsLog, 'turquoise'), chalk.magenta('From'), chalk.green(pushname), chalk.yellow(`[ ${m.sender.replace('@s.whatsapp.net', '')} ]`), chalk.blueBright('IN'), chalk.green(groupName))
             }
