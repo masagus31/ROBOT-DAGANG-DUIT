5445902385b079a07cc8f5f829c51ef339425553
Agus Pujianto <53058544+masagus31@users.noreply.github.com>
2023-05-11 22:18:04 +0700

    Update sansekai.js

--- sansekai.js
+++ sansekai.js
@@ -148,6 +148,7 @@ module.exports = sansekai = async (client, m, chatUpdate, store) => {
                     const response = await openai.createCompletion({
                         model: "text-davinci-003",
                         prompt: prompt_template,
+                        allowed: allowed_links,
                         temperature: 0.9,
                         max_tokens: 3000,
                         top_p: 1,
